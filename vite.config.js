680b827529f4b51e7c6581d08d3f1ae49654e912
Polina Kasich <poli.key@yandex.ru>
2025-02-13 15:13:43 +0500

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/fuse8_2025/", // YOUR REPO NAME HERE
+});
